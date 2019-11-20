032fc2739b7e8706808a4b6468b64b9c1bd61f31
CJBuchel <connorbuchel@gmail.com>
2019-11-20 17:02:13 +0800

    Added 2020CodeBase to re fucking write 5663:

---
+++ 4788/src/main/cpp/Robot.cpp
@@ -0,0 +1,39 @@
+#include "Robot.h"
+#include "strategy/MPStrategy.h"
+
+#include <math.h>
+#include <iostream>
+
+#include <cameraserver/CameraServer.h>
+
+#include <frc/DriverStation.h>
+
+using namespace frc;
+using namespace curtinfrc;
+
+double lastTimestamp;
+
+void Robot::RobotInit() {
+  
+}
+
+
+void Robot::RobotPeriodic() {
+  // In periodic mode. Continuous looped code
+}
+
+void Robot::DisabledInit() {
+  InterruptAll(true);
+}
+
+void Robot::AutonomousInit() {
+  // Schedule(std::make_shared<PathfinderMPStrategy>(*drivetrain, robotmap.drivetrain.gainsPathfinder, "5333", "d2_bM"));
+}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {
+}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
